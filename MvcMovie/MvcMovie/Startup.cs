2490a7b1fa4eceadfa8f6a996dec2847027fff6c
kb21284 <s21284@pjwstk.edu.pl>
2022-06-09 20:56:49 +0200

    add search

--- MvcMovie/MvcMovie/Startup.cs
+++ MvcMovie/MvcMovie/Startup.cs
@@ -56,6 +56,7 @@ namespace MvcMovie
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+
             });
         }
     }
